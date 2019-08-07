ad1ef91df9813bdde30db82e625aa884d2a330ba
delProfundo <bruno@hypermedia.tech>
2019-08-07 19:55:04 +1000

    some changes for the .d.ts file generation

--- src/lib/base64.ts
+++ src/lib/base64.ts
@@ -2,13 +2,13 @@ const encodeBuffer = ( buffer: Buffer ) : string => buffer.toString( "base64" );
 const encodeString = ( string: string ): string  => encodeBuffer( Buffer.from( string ));
 const encodeData = ( data: object ): string => encodeString( JSON.stringify( data ));
 
-const encode = ( data: Buffer|string|object ): string => {
+export const encode = ( data: Buffer|string|object ): string => {
   if( Buffer.isBuffer( data )) return encodeBuffer( data );
   if( typeof data === "string" ) return encodeString( data );
   return encodeData( data );
 };
 
-const decode = ( string: string ) : object|string => {
+export const decode = ( string: string ) : object|string => {
   const decoded = Buffer.from( string, "base64").toString();
   try {
     return JSON.parse( decoded );
@@ -16,5 +16,3 @@ const decode = ( string: string ) : object|string => {
     return decoded
   }
 };
-
-module.exports = { encode, decode };
