3ded5c9bf01be67f814aa714dc3c380e7a97b17e
delprofundoy <bruno@hypermedia.tech>
2021-01-26 14:12:13 +1000

    fixed some missing types

--- src/lib/jose.ts
+++ src/lib/jose.ts
@@ -4,7 +4,7 @@ const { encode, decode } = require('./base64');
 
 const encrypt = async ( raw: object, publicKey: JWK.Key ) : Promise<JWE.Encryptor> => {
   if (!raw) throw new Error('Missing raw data.');
-  const buffer = Buffer.from(JSON.stringify(raw));
+  const buffer = Buffer.from(JSON.stringify(raw), 'utf8');
   const encrypted = await JWE.createEncrypt({}, publicKey)
     .update(buffer)
     .final();
